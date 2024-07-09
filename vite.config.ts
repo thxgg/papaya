97e03c1e8e09408034302bf9fc240f5a37c372f3
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2024-07-10 02:38:47 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { URL, fileURLToPath } from 'node:url'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VueDevTools()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
